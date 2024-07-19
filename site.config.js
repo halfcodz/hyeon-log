05abcbbf4847ec03b1564967e3708cd444014add
kkr010128 <kkr010128@icloud.com>
2024-07-19 18:19:28 +0900

    Revert "Update site.config.js"
    This reverts commit 081ad27a34383ed1e960447703582708dca41489.

--- site.config.js
+++ site.config.js
@@ -1,32 +1,32 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "PANG",
+    name: "morethanmin",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "컴퓨터공학과 3학년",
+    role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "kkr010128@icloud.com",
-    linkedin: "kkr010128",
-    github: "kkr010128",
-    instagram: "kk_0128_",
+    email: "morethanmin.dev@gmail.com",
+    linkedin: "morethanmin",
+    github: "morethanmin",
+    instagram: "",
   },
   projects: [
     {
-      name: `pang-log`,
-      href: "https://github.com/kkr010128",
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "pang-log",
-    description: "개발자가 되기 위한 공부노트",
+    title: "morethan-log",
+    description: "welcome to morethan-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://pang-log.vercel.app",
-  since: 2023, // If leave this empty, current year will be used.
-  lang: "en-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://morethan-log.vercel.app",
+  since: 2022, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
@@ -42,7 +42,7 @@ const CONFIG = {
     },
   },
   googleSearchConsole: {
-    enable: true,
+    enable: false,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
@@ -54,18 +54,18 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: false,
+    enable: true,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
-      label: "💬 댓글",
+      label: "💬 Utterances",
     },
   },
   cusdis: {
-    enable: true,
+    enable: false,
     config: {
       host: "https://cusdis.com",
-      appid: "7ed39862-081a-47e9-970d-b0fae35449d6", // Embed Code -> data-app-id value
+      appid: "", // Embed Code -> data-app-id value
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
