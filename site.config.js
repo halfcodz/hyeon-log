16083ddb39a6401452b897d38a4ed7fdb91fc066
halfcodz <jeongusrn@gmail.com>
2024-12-26 23:28:37 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "halfcodz",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "to be code master",
     bio: "머리는 차갑게 가슴은 뜨겁게",
     email: "jeongusrn@gmail.com",
